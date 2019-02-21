553ed3f8180905b7453cae4db31cb17cd1bcbb82
Himanshu Chauhan <31559177+himanshuchauhan981@users.noreply.github.com>
2019-02-21 16:28:16 +0530

    Create users.js

---
+++ models/users.js
@@ -0,0 +1,25 @@
+const mongoose = require('mongoose')
+mongoose.connect('mongodb://blogging_blogs:bloggingAdmin0018@ds161024.mlab.com:61024/blogging_blogs',{ useNewUrlParser: true })
+
+const userSchema = mongoose.Schema({
+   username:String,
+   email:String,
+   password:String
+})
+
+const User = mongoose.model('user_data',userSchema)
+module.exports = User
+
+module.exports.getExistingUsername = (inputUsername,callback) =>{
+   query = {username:inputUsername}
+   User.findOne(query,callback)
+}
+
+module.exports.createNewUser = (newUser,callback) =>{
+   newUser.save(newUser,callback)
+}
+
+module.exports.getExistingEmail = (inputEmail,callback) =>{
+   query = {email:inputEmail}
+   User.findOne(query,callback)
+}
