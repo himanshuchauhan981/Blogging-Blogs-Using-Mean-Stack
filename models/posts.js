a3934c399bf7cbd741fafca3088cf91200cac2cb
Himanshu Chauhan <31559177+himanshuchauhan981@users.noreply.github.com>
2019-02-22 01:11:00 +0530

    Create posts.js

---
+++ models/posts.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose')
+mongoose.connect('mongodb://blogging_blogs:bloggingAdmin0018@ds161024.mlab.com:61024/blogging_blogs',{ useNewUrlParser: true })
+
+const postSchema = mongoose.Schema({
+   postTitle : String,
+   postContent : String,
+   postDate : String,
+   postAuthor : String
+})
+
+module.exports = mongoose.model('posts',postSchema)
+const Post = mongoose.model('posts',postSchema)
+module.exports.Posts = Post;
