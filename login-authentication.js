7c941f7e97298640d667c3ce549a737bc76fcb33
Himanshu Chauhan <31559177+himanshuchauhan981@users.noreply.github.com>
2019-02-28 10:49:39 +0530

    Update login-authentication.js

--- login-authentication.js
+++ login-authentication.js
@@ -3,6 +3,9 @@ const router = express.Router()
 const app = express()
 const session = require('express-session')
 const cookieParser = require('cookie-parser')
+const expressRouter = require('express-promise-router')
+
+const GooglePlusTokenStrategy = require('passport-google-plus-token')
 
 app.use(cookieParser())
 app.use(session({
@@ -20,14 +23,36 @@ router.use(passport.session());
 //Setting routes
 const User = require('./models/users')
 
+//Google OAuth Strategy
+passport.use('googleToken',new GooglePlusTokenStrategy({
+   clientID :'192284820445-68v42tsa08bptglg5330ol75f3oc2v4o.apps.googleusercontent.com',
+   clientSecret : 'q4WHioSJTPlsIuNkDXI6e6df'
+}, async (accessToken, refreshToken, profile, done) =>{
+   console.log('Access Token : ',accessToken);
+   console.log('Refresh Token : ',refreshToken);
+   console.log('Profile : ',profile);
+   const newUser = new User({
+      method:'google',
+      google:{
+         id:profile.id,
+         email:profile.emails[0].value
+      }
+   })
+   User.createNewUser(newUser,()=>{
+      console.log('User created')
+   })
+
+}));
+
 passport.use(new LocalStrategy(
    function(username, password, done){
       User.getExistingUsername(username, (err,user) => {
+         console.log(user)
          if(err) throw err;
          if(!user){
             return done(null,false,{message:'Invalid Username'});
          }
-         if(user.password == password){
+         if(user.local.password == password){
             return done(null,user)
          }
          else{
