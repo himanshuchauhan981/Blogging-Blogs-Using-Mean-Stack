48190f899acee4222a53e11fb13508b2a3275a97
Himanshu Chauhan <31559177+himanshuchauhan981@users.noreply.github.com>
2019-02-28 10:50:06 +0530

    Update route.js

--- routes/route.js
+++ routes/route.js
@@ -3,6 +3,10 @@ const app = express()
 const router = express.Router()
 const User = require('../models/users')
 const Post = require('../models/posts')
+
+const passport = require('passport')
+const expressRouter = require('express-promise-router')();
+
 router.get('/',function(request,response){
    Post.getAllPostData((err,post_data) =>{
       len = post_data.length
@@ -26,35 +30,8 @@ router.get('/createPost',(request,response) =>{
    return response.render('createPost.ejs',{titlePage:'Create Post - Blogging Blogs'})
 })
 
-router.post('/validate',(request,response) => {
-   inputUsername = request.body.inputUsername
-   inputEmail = request.body.inputEmail
-   inputPassword = request.body.inputPassword
-   const newUser = new User({
-      username:inputUsername,
-      email:inputEmail,
-      password:inputPassword
-   })
-   User.getExistingUsername(inputUsername, (err,userName) => {
-      if(userName){
-         request.flash('danger','Username Existed')
-         return response.render('signup',{titlePage:'Sign Up - Blogging Blogs'})
-      }
-      else{
-         User.getExistingEmail(inputEmail,(err,userEmail) => {
-            if(userEmail){
-               request.flash('danger','Email ID existed')
-               return response.render('signup',{titlePage:'Sign Up - Blogging Blogs'})
-            }
-            else{
-               User.createNewUser(newUser,() =>{
-                  return response.render('login.ejs',{titlePage:'Login - Blogging Blogs'})
-               });
-            }
-         })
-      }
-   })
-})
+router.route('/oauth/google').post(passport.authenticate('googleToken',{session:false}))
+
 
 
 module.exports = router
