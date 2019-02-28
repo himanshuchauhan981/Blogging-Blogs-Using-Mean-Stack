8a40486debfe90b6545e802025bb6b639061a527
Himanshu Chauhan <31559177+himanshuchauhan981@users.noreply.github.com>
2019-02-28 19:37:45 +0530

    Create keys.js

---
+++ keys.js
@@ -0,0 +1,9 @@
+module.exports = {
+   google:{
+      clientID:'15339551386-aqb42gsniuq21v9oe3s7554iesabek6e.apps.googleusercontent.com',
+      clientSecret:'5CqoxUdr3X6dDcAyHBnUYthc'
+   },
+   session:{
+      cookieKey:'qwertyuiopasdfghjklzxcvbnm'
+   }
+}
