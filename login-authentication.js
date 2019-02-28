de6978ad2dfe63e2cf53199915154c9dcc40bff4
Himanshu Chauhan <31559177+himanshuchauhan981@users.noreply.github.com>
2019-02-28 23:21:27 +0530

    Update login-authentication.js

--- login-authentication.js
+++ login-authentication.js
@@ -1,22 +1,22 @@
 const express = require('express')
 const router = express.Router()
 const app = express()
-const session = require('express-session')
-const cookieParser = require('cookie-parser')
-const cookieSession = require('cookie-session')
+// const session = require('express-session')
+// const cookieParser = require('cookie-parser')
+// const cookieSession = require('cookie-session')
 
 const keys = require('./keys')
 
-app.use(cookieParser())
-app.use(session({
-   secret:'blogging_blogs',
-   saveUninitialized: true,
-   resave:true
-}))
-app.use(cookieSession({
-   maxAge:24*60*60*1000,
-   keys:[keys.session.cookieKey]
-}))
+// app.use(cookieParser())
+// app.use(session({
+//    secret:'blogging_blogs',
+//    saveUninitialized: true,
+//    resave:true
+// }))
+// app.use(cookieSession({
+//    maxAge:24*60*60*1000,
+//    keys:[keys.session.cookieKey]
+// }))
 
 const passport = require('passport')
 const LocalStrategy = require('passport-local').Strategy
@@ -25,9 +25,22 @@ const GoogleStrategy =  require('passport-google-oauth20')
 router.use(passport.initialize())
 router.use(passport.session())
 
+// app.use(passport.initialize())
+// app.use(passport.session())
+
 //Setting routes
 const User = require('./models/users')
 
+passport.serializeUser(function(request,user, done) {
+   request.session.currentUser = request.body.username
+   done(null, user.id);
+});
+
+passport.deserializeUser(function(id, done) {
+   User.getByID(id, function(err, user) {
+      done(err, user);
+   });
+});
 
 //Passport Google Strategy
 passport.use(
@@ -35,28 +48,27 @@ passport.use(
       callbackURL:'/auth/google/redirect',
       clientID:keys.google.clientID,
       clientSecret:keys.google.clientSecret
-   }, (accessToken, refreshToken, profile, done) =>{
+   },(accessToken, refreshToken, profile, done) =>{
       User.getUsersFromGoogleSignUp(profile.id,(err,currentUser) =>{
          if(currentUser){
             console.log('user already existed')
-            done(null,currentUser);
+            return done(null,currentUser)
          }
          else{
-            const newUser = new User({
+            new User({
                method:'google',
                google:{
                   id:profile.id,
                   username:profile.displayName
                }
-            }).save().then((newUser) => {
+            }).save().then((newUser) =>{
                console.log('New User Created : '+newUser)
-               return done(null,newUser);
+               return done(null, newUser)
             })
          }
       })
    })
 )
-
 passport.use(new LocalStrategy(
    function(username, password, done){
       User.getExistingUsername(username, (err,user) => {
@@ -75,16 +87,6 @@ passport.use(new LocalStrategy(
    }
 ));
 
-passport.serializeUser(function(request,user, done) {
-   request.session.currentUser = request.body.username
-   done(null, user.id);
-});
-
-passport.deserializeUser(function(id, done) {
-   User.getByID(id, function(err, user) {
-      done(err, user);
-   });
-});
 
 router.post('/login',
 passport.authenticate('local',{successRedirect:'/', failureRedirect:'/login', failureFlash: true}),
