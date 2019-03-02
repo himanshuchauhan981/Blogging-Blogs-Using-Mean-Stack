c4595dbd12c051813a4717de7b7864ff7cbf549c
Himanshu Chauhan <31559177+himanshuchauhan981@users.noreply.github.com>
2019-03-02 23:05:42 +0530

    Update route.js

--- routes/route.js
+++ routes/route.js
@@ -1,11 +1,13 @@
 const express = require('express')
+const bodyParser = require('body-parser')
 const app = express()
 const router = express.Router()
 const User = require('../models/users')
 const Post = require('../models/posts')
-
 const passport = require('passport')
-//const passportSetup = require('.././login-authentication')
+
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended:false}))
 
 router.get('/',function(request,response){
    Post.getAllPostData((err,post_data) =>{
@@ -30,8 +32,12 @@ router.get('/createPost',(request,response) =>{
    return response.render('createPost.ejs',{titlePage:'Create Post - Blogging Blogs'})
 })
 
-router.get('/viewPost',(request,response) =>{
-   return response.render('viewPost.ejs',{titlePage:'View Post - Blogging Blogs'})
+router.post('/viewPost',(request,response) =>{
+   data = request.body
+   const keys = Object.keys(data)
+   Post.getPostData(keys[0],(err,user)=>{
+      return response.render('viewPost.ejs',{titlePage:'View Post - Blogging Blogs',data:user,postUser:request.session.currentUser})
+   })
 })
 
 router.get('/google',passport.authenticate('google',{
@@ -41,4 +47,6 @@ router.get('/google',passport.authenticate('google',{
 router.get('/auth/google/redirect',passport.authenticate('google'),(req,res) =>{
    res.redirect('/')
 })
+
+
 module.exports = router
