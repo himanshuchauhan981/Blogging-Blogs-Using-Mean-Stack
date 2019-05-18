f55199f35b1b04588b60defccf40ea9176c43d01
himanshuchauhan981 <himanshuchauhan981@gmail.com>
2019-05-18 08:12:36 +0530

    Create keys.js

---
+++ config/keys.js
@@ -0,0 +1,16 @@
+module.exports = {
+   linkedin:{
+      clientID:'81th4yjwzdd9c2',
+      clientSecret:'OuuPO9UzhWFYlk3s'
+   },
+   google:{
+      clientID:'15339551386-aqb42gsniuq21v9oe3s7554iesabek6e.apps.googleusercontent.com',
+      clientSecret:'5CqoxUdr3X6dDcAyHBnUYthc'
+   },
+   session:{
+      cookieKey:'qwertyuiopasdfghjklzxcvbnm'
+   },
+   mongoURI:{
+      mongokey:'mongodb://blogging_blogs:bloggingAdmin0018@ds161024.mlab.com:61024/blogging_blogs'
+   }
+}
