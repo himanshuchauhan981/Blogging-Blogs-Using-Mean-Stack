489c33908ccae95dd37469620f656ee4f7e9504d
himanshuchauhan981 <himanshuchauhan981@gmail.com>
2019-05-22 10:38:52 +0530

    Update javascript.js

--- public/js/javascript.js
+++ public/js/javascript.js
@@ -1,24 +1,9 @@
-function changeLikeMode(){
-   checkColor = document.getElementById('likebutton');
-   // console.log(checkColor)
-   if(checkColor.style.color =="blue"){
-      checkColor.style.color = "black";
-   }
-   else{
-      checkColor.style.color= "blue";
-   }
-}
-
-function showpassword(){
-   var click = document.getElementById('showpasswordClick')
+showPassword = (data)=>{
+   var click = document.getElementById(data);
    if(click.type=="password"){
       click.type="text";
    }
-   else{
+   else{ 
       click.type="password";
    }
-}
-
-function getFocus(){
-    document.getElementById("comment").focus();
 }
