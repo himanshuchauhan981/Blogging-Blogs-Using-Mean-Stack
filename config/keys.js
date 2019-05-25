df94c879a4d6a0c22a38a2804ed356df5010bee0
Himanshu Chauhan <31559177+himanshuchauhan981@users.noreply.github.com>
2019-05-25 23:19:59 +0530

    Update keys.js

--- config/keys.js
+++ config/keys.js
@@ -1,7 +1,7 @@
 module.exports = {
    linkedin:{
-      clientID:'81th4yjwzdd9c2',
-      clientSecret:'OuuPO9UzhWFYlk3s'
+      clientID:'81opl3bnx6svjx',
+      clientSecret:'81opl3bnx6svjx'
    },
    google:{
       clientID:'192284820445-jap02f5afgsgt1ijh2nbgj48pb22193r.apps.googleusercontent.com',
