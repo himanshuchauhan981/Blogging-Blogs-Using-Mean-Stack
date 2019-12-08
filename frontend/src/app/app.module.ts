94de23e005b2a4ded24c4ad2d057d688a4d63398
himanshu1365 <himanshu1365.cse16@chitkara.edu.in>
2019-12-08 18:17:50 +0530

    added login and signup components + added frontend routes

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,24 +1,33 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
+import { RouterModule } from '@angular/router'
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { NavbarComponent } from './header/navbar/navbar.component';
 import { CarousalComponent } from './header/carousal/carousal.component';
+import { LoginComponent } from './login/login.component';
+import { SignupComponent } from './signup/signup.component'
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     NavbarComponent,
-    CarousalComponent
+    CarousalComponent,
+    LoginComponent,
+    SignupComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    NgbModule
+    NgbModule,
+    RouterModule.forRoot([
+      { path: 'login', component: LoginComponent},
+      { path: 'signup', component: SignupComponent}
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
