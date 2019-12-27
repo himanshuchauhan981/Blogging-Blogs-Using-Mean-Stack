0e341a04562e3e363390eb5410e608f0dffd4625
Himanshu Chauhan <himanshuchauhan981@gmail.com>
2019-12-27 23:52:03 +0530

    completed frontend design for showing comments

--- frontend/src/app/view-post/view-post.component.ts
+++ frontend/src/app/view-post/view-post.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core'
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core'
 import { ActivatedRoute } from '@angular/router'
 import { PostService } from '../service/post.service'
 
@@ -12,6 +12,11 @@ export class ViewPostComponent implements OnInit {
 	post = {}
 
 	constructor(private activatedRoute: ActivatedRoute, private postService: PostService) { }
+
+	@ViewChild("comment",{static: false}) nameField: ElementRef;
+	focusCommentInput() :  void{
+		this.nameField.nativeElement.focus()
+	}
 
 	ngOnInit() {
 		let postID = this.activatedRoute.snapshot.params.id
