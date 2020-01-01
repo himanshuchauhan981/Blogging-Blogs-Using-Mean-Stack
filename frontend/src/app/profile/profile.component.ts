8096eb9aaff4b1fd44d13f4b20abdb55924f7824
Himanshu Chauhan <himanshuchauhan981@gmail.com>
2020-01-01 12:31:49 +0530

    added frontend design to profile component

--- frontend/src/app/profile/profile.component.ts
+++ frontend/src/app/profile/profile.component.ts
@@ -1,15 +1,27 @@
 import { Component, OnInit } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog'
+
+import { ProfileDialogBoxComponent } from './profile-dialog-box/profile-dialog-box.component'
 
 @Component({
-  selector: 'app-profile',
-  templateUrl: './profile.component.html',
-  styleUrls: ['./profile.component.css']
+	selector: 'profile',
+	templateUrl: './profile.component.html',
+	styleUrls: ['./profile.component.css']
 })
 export class ProfileComponent implements OnInit {
 
-  constructor() { }
+	heading: string
 
-  ngOnInit() {
-  }
+	constructor(public matDialog: MatDialog) { }
+
+	ngOnInit() {
+	}
+
+	openDialogBox(heading) {
+		this.matDialog.open(ProfileDialogBoxComponent, {
+			width: '250px',
+			data : { heading: heading}
+		})
+	}
 
 }
