d7ebb0b584b582aeb8c6610624dbdcc0528786a1
himanshuchauhan981 <himanshuchauhan981@gmail.com>
2020-01-02 21:55:45 +0530

    redesigned profile component

--- frontend/src/app/posts/view-all-posts/view-all-posts.component.ts
+++ frontend/src/app/posts/view-all-posts/view-all-posts.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core'
-import { Router, ActivatedRoute,ActivatedRouteSnapshot } from '@angular/router'
+import { Router, ActivatedRoute } from '@angular/router'
 import { MatSnackBar } from '@angular/material/snack-bar'
 
 import { PostService } from '../../service/post.service'
