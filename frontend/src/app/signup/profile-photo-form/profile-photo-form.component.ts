58cf962f4381d81fd30b9909d6f5ff35ae6f3a05
himanshuchauhan981 <himanshuchauhan981@gmail.com>
2020-01-06 13:23:56 +0530

    added frontend design for profile form component

--- frontend/src/app/signup/profile-photo-form/profile-photo-form.component.ts
+++ frontend/src/app/signup/profile-photo-form/profile-photo-form.component.ts
@@ -1,15 +1,48 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-profile-photo-form',
-  templateUrl: './profile-photo-form.component.html',
-  styleUrls: ['./profile-photo-form.component.css']
+	selector: 'profile-photo-form',
+	templateUrl: './profile-photo-form.component.html',
+	styleUrls: ['./profile-photo-form.component.css']
 })
-export class ProfilePhotoFormComponent implements OnInit {
+export class ProfilePhotoFormComponent {
 
-  constructor() { }
+	constructor() { }
 
-  ngOnInit() {
-  }
+	uploadedFiles: Array<File>
 
+	fileType: Array<String> = ['image/jpeg', 'image/jpg', 'image/png']
+
+	uploadFileText: String = 'Upload Profile Pic'
+
+	fileUploadColor: String = "accent"
+
+	defaultImgSrc: string = 'https://i.stack.imgur.com/X9JD4.png?s=136&g=1'
+
+	fileChange(element) {
+		console.log(element.target.files)
+		let filetype = element.target.files[0].type
+		if (this.fileType.indexOf(filetype) >= 0) {
+			var reader = new FileReader();
+
+			reader.onload = (event: any) => {
+				this.defaultImgSrc = event.target.result;
+			}
+
+			reader.readAsDataURL(element.target.files[0]);
+
+			this.uploadedFiles = element.target.files
+			this.uploadFileText = "File Uploaded"
+			this.fileUploadColor = "primary"
+		}
+		else {
+			this.uploadFileText = "Invalid File"
+			this.fileUploadColor = "warn"
+		}
+
+	}
+
+	uploadFile() {
+		document.getElementById('upload').click()
+	}
 }
