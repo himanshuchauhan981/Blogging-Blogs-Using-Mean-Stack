3b325176f9301cb22a741addc0d71193ad2ce72e
himanshuchauhan981 <himanshuchauhan981@gmail.com>
2020-07-10 11:44:30 +0530

    changed navbar and removed unused components

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -8,7 +8,10 @@ import { StorageServiceModule } from 'angular-webstorage-service'
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
 import { MatCardModule } from '@angular/material/card'
 import { MatFormFieldModule } from '@angular/material/form-field'
-import { MatInputModule,MatIconModule,MatButtonModule,MatCheckboxModule } from '@angular/material'
+import { MatInputModule } from '@angular/material/input'
+import { MatIconModule } from '@angular/material/icon'
+import { MatButtonModule } from '@angular/material/button'
+import { MatCheckboxModule } from '@angular/material/checkbox'
 import { MatSnackBarModule } from '@angular/material/snack-bar'
 import { MatExpansionModule } from '@angular/material/expansion'
 import { MatTooltipModule } from '@angular/material/tooltip'
@@ -19,9 +22,8 @@ import { InfiniteScrollModule } from 'ngx-infinite-scroll'
 
 import { AppRoutingModule } from './app-routing.module'
 import { AppComponent } from './app.component'
-import { HeaderComponent } from './header/header.component'
-import { NavbarComponent } from './header/navbar/navbar.component'
-import { CarousalComponent } from './header/carousal/carousal.component'
+import { NavbarComponent } from './navbar/navbar.component'
+import { CarousalComponent } from './carousal/carousal.component'
 import { LoginComponent } from './login/login.component'
 import { SignupComponent } from './signup/signup.component'
 import { HomeComponent } from './home/home.component'
@@ -41,7 +43,6 @@ import { ErrorIntercept } from './error.interceptor'
 @NgModule({
 	declarations: [
 		AppComponent,
-		HeaderComponent,
 		NavbarComponent,
 		CarousalComponent,
 		LoginComponent,
---
+++ frontend/src/app/navbar/navbar.component.ts
@@ -0,0 +1,55 @@
+import { Component, OnInit } from '@angular/core'
+import { Router } from '@angular/router'
+
+import { AuthGuardService } from '../service/auth-guard.service'
+import { ProfileService } from 'src/app/service/profile.service'
+import { UserService } from 'src/app/service/user.service'
+
+@Component({
+	selector: 'navbar',
+	templateUrl: './navbar.component.html',
+	styleUrls: ['./navbar.component.css']
+})
+export class NavbarComponent implements OnInit{
+
+	constructor(
+		private userService: UserService,
+		private profileService: ProfileService,
+		private router: Router,
+		private authGuardService: AuthGuardService
+	) { }
+
+	loginStatus : Boolean = false
+
+	public isMenuCollapsed = true
+
+	public username
+
+	userList = []
+
+	capitalizaFirstAndLastName(fullName){
+		return fullName.toLowerCase().replace( /\b./g, function(a){ return a.toUpperCase(); } );
+	}
+
+	logout(){
+		this.userService.logout()
+		this.router.navigate(['login'])
+	}
+
+	ngOnInit(){
+		this.userService.loginObservable.subscribe(value =>{
+			this.loginStatus = value
+			this.username = this.authGuardService.currentUser
+		})
+	}
+
+	redirectToUser($event,input){
+		let userId = $event.item._id
+		this.profileService.username(userId).subscribe(
+			(response:any) => {
+				this.router.navigate([`/profile/${response.username}`])
+				input.value = ''
+			}
+		)
+	}
+}
